10848ee5b9a3a6d08eba763d30f60d23acd2de1e
Drew <drw.spencer@gmail.com>
2019-09-23 17:00:11 -0500

    Implementing URL Hash

---
+++ src/components/PathConsole.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>This is locn: {{this.$route.params.locnid}}</h1>
+</template>
+
+<script>
+import store from '../store.js'
+import axios from 'axios'
+
+export default {
+  name: 'PathConsole',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
