a33bf64a01cdf498145c42cc6f5131fc171c2753
Drew <drw.spencer@gmail.com>
2019-09-27 15:54:22 -0500

    v 1 test built

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="abouttext">
+    <h1>Welcome to Path Slide Tracker</h1>
+    <br>
+    <div class="abouttextparagraph">
+    <P>You will need to access your individual slide tracker at the address given to you by MAWD it.
+      Basically, you will need to go to the site www.slide-tracker.com/#/locn/04780183.  If your sides
+      are assigned to a hospital as opposed to a specific pathologist, the address will be that hospital
+      abbreviation instead of the unique id.  For example www.slide-tracker.com/#/locn/NKCH
+    </P>
+    <p>
+      If you navigate to a page from here, you will need to refresh your browser once you get there.
+      For future access, bookmark the page that has the full location id in it.
+    </p>
+    <p>See the about page for more details.</p>
+    </div>
   </div>
 </template>
 
