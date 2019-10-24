84e9873a661b56b2620073ea0910b19ab9334ec8
Drew <drw.spencer@gmail.com>
2019-10-24 09:46:34 -0500

    Push .env.local.js

---
+++ src/.env.local.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //  Prod
+    //  apiURL: 'http://10.24.4.9:2081',
+    //  Test
+    //  apiURL: 'http://10.24.4.9:2082',
+    //  Local Test
+    apiURL: 'http://localhost:2081',
+    //  Prod URL
+    //  apiURL: 'xxx',
+    apitoken: 'xxx'
+}
