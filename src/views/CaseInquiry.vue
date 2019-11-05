e8df3a0cd0ee1429af0796fab3b439b37dca8123
Drew <drw.spencer@gmail.com>
2019-11-05 17:09:36 -0600

    Inquiry

---
+++ src/views/CaseInquiry.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="abouttext">
+      <h2>Case Inquiry</h2>
+      <br>
+      <LookupSlideDetails> </LookupSlideDetails>
+      
+      <br>
+      <div class="abouttextparagraph">
+      </div>
+    </div>
+</template>
+
+<script>
+import LookupSlideDetails from '../components/LookupSlideDetails'
+
+export default {
+  name: 'CaseInquiry',
+  components:
+  {
+     LookupSlideDetails
+  },
+  data() {
+    return {
+     // strSlideDistLoc: null,
+    }
+  },
+  mounted() {
+  },
+}
+</script>
